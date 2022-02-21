b23a3d0ca86dc3c9f08b4c8b8913ed32057ad8b4
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-02-21 09:52:10 +0000

    p3original.c

---
+++ p2original.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void input_line(float *x1,float *y1)
+{
+  printf("enter two numbers\n");
+  scanf("%f %f\n",x1,y1);
+}
+int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
+{
+  while()
+}
---
+++ p3original.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int input_number()
+{
+  int x;
+  printf("enter a number\n");
+  scanf("%d",&x);
+  return x;
+}
+int is_prime(int n)
+{
+  int prime,i;
+  for(int i=1;i<n/2;i++)
+  {
+    if(n%i==0)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  return prime;
+}
+void output(int n,int prime)
+{
+  if(prime==1)
+  {
+    printf("%d is not a prime number\n",n);
+  }
+  else
+  {
+    printf("%d is a prime number\n",n);
+  }
+}
+int main()
+{
+  int n,prime;
+  n=input_number();
+  prime=is_prime(n);
+  output(n,prime);
+  return 0;
+}
