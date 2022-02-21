0c0d9f01c5425cc42182ea21f274540f6006212a
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-02-21 09:22:01 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,23 @@
-
+#include<stdio.h>
+#include<math.h>
+void input(float *x1,float *y1,float *x2,float*y2)
+{
+  printf("enter a number\n");
+  scanf("%f%f%f%f\n",x1,y1,x2,y2);
+}
+void find_distance(float x1,float y1,float x2,float y2,float *area)
+{
+  *area=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+void output(float area)
+{
+  printf("the distance between two points is %f\n",area);
+}
+int main()
+{
+  float x1,y1,x2,y2,area;
+  input(&x1,&y1,&x2,&y2);
+  find_distance(x1,y1,x2,y2,&area);
+  output(area);
+  return 0;
 }
