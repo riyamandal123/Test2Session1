741d9967fa84b70c62325fa56a9143ee85761b93
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-02-25 04:43:17 +0000

    p3original.c

--- p3original.c
+++ p3original.c
@@ -9,7 +9,7 @@ int input_number()
 int is_prime(int n)
 {
   int prime,i;
-  for(int i=1;i<=n/2;i++)
+  for(int i=2;i<=n/2;i++)
   {
     if(n%i==0)
     {
@@ -24,7 +24,7 @@ int is_prime(int n)
 }
 void output(int n,int prime)
 {
-  if(prime==1)
+  if(prime==0)
   {
     printf("%d is not a prime number\n",n);
   }
---
+++ p5original.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the array size\n");
+  scanf("%d",&n);
+  return n;
+}
+void init _array(int n,int a[n])
+{
+  for(int i=0;i<n;i++)
+    a[i]=0;
+    a[i]=1;
+}
+void erotosthenes_sieve(int n,int a[n])
+{
+  int
+}
