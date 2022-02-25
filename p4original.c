f7ba00ef025536f60d2f12e1b7b36e620eae7b20
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-02-25 04:11:23 +0000

    p4original.c

--- p4original.c
+++ p4original.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 int input()
 {
-  int x;
-  printf("")
+  int n;
+  printf("enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int a=0,b=1,c;
+  for(int i=2;i<n;i++)
+    {
+      c=a+b;
+      a=b;
+      b=c;
+    }
+  return c;
+}
+void output(int n,int fibo)
+{
+  printf("%d\n",fibo);
+}
+int main()
+{
+  int n,fibo;
+  n=input();
+  fibo=find_fibo(n);
+  output(n,fibo);
+  return 0;
 }
