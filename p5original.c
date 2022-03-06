ca23cebdb5db60463b75beccf00b66db04cec077
Riya Mandal <96898236+riyamandal123@users.noreply.github.com>
2022-03-06 08:23:49 +0000

    p5original.c

--- p5original.c
+++ p5original.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<math.h>
 int input_array_size()
 {
   int n;
@@ -6,13 +7,39 @@ int input_array_size()
   scanf("%d",&n);
   return n;
 }
-void init _array(int n,int a[n])
+void init_array(int n,int a[n])
 {
   for(int i=0;i<n;i++)
-    a[i]=0;
-    a[i]=1;
+    { 
+      a[i]=1;
+    } 
+    a[1]=0;
 }
 void erotosthenes_sieve(int n,int a[n])
 {
-  int
+  int i=0;
+  while(i<sqrt(n))
+    {
+      /*make the multiples of i zero*/
+      while(a[i]==0 && i<sqrt(n))i++;
+      for(int k=i+i;k<n;k+=i)
+        a[k]=0;
+      i++;
+    }
+}
+void output(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+    printf("%d",a[i]);
+  printf("the prime numbers between erosthenes sieve is %d\n",n);
+}
+int main()
+{
+  int n;
+  n=input_array_size();
+  int a[n];
+  init_array(n,a);
+  erotosthenes_sieve(n,a);
+  output(n,a);
+  return 0;
 }
